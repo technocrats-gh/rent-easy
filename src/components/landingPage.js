f88baec63a89b0906f4ee66e96886c96c99678b1
Amankwah Amoako Kwaku <andarmaas3@gmail.com>
2024-11-26 13:21:47 +0000

    landing-page redesign

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -9,33 +9,72 @@ import emailIcon from '../images/email.png'
 import locationIcon from '../images/locs.png'
 import { CustomDialog } from '../utils/dialog';
 import { useNavigate, Link } from 'react-router-dom';
+import { Button } from 'primereact/button';
 
 const LandingPage = (props) => {
     const [viewLandingContact, setViewLandingContact] = useState(false);
 
     const hideContactDia = () => setViewLandingContact(false);
 
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+
     return (
-        <div className="landing-page">
-            <header>
+        <div className="landing-page" >
+            <div className='grid col-12 mb-6'>
+                <div className='flex col-10 logo-div' onClick={() => navigate("/")}>
+                    <img src={logo} alt="Rent Easy Logo" className="logo-img" />
+                    <div className="flex flex-column flex-start" >
+                        <span className='logo-txt'> Rent Easy</span>
+                        <span className='logo-txt-2'> easy rentals</span>
+                    </div>
+                </div>
+                <ul className="links col-2">
+                    <li onClick={() => setViewLandingContact(true)}>Contact Us</li>
+                </ul>
+            </div>
+
+            <div className='grid col-12 hero'>
+                <div className="info col-12 md:col-6 lg:col-6">
+                    <span className='first-title'>Looking For A Place to Call Home?</span>
+                    <span className='second-title'>Get a favorable home space from just a click on your phone. No more stress and long talk</span>
+                    {/* <div className='flex gap-5 mt-5 '>
+                        <Button className='btn' onClick={() => navigate("/HomePage")}>View Listings</Button>
+                        <Button className='btn'><StartLogin /></Button>
+                    </div> */}
+                </div>
+                <div className="image col-12 md:col-6 lg:col-6" >
+                    <img src={apartImg} alt="Apartment Image" className='landing-img' />
+                </div>
+            </div>
+
+            <div className='grid'>
+                <div className='flex gap-5 land-btn'>
+                    <Button className='btn' onClick={() => navigate("/HomePage")}>View Listings</Button>
+                    <Button className='btn'><StartLogin /></Button>
+                </div>
+            </div>
+            {/* </div> */}
+            {/* <header className='col-12'>
                 <div className="container">
                     <div className='flex' onClick={() => navigate("/")}>
                         <img src={logo} alt="Rent Easy Logo" className="logo-img" />
-                        <div style={{ display: 'flex', flexDirection: "column", alignItems: "flex-start" }}>
+                        <div className="flex flex-column flex-start" >
                             <span className='logo-txt'> Rent Easy</span>
                             <span className='logo-txt-2'> easy rentals</span>
                         </div>
                     </div>
                     <ul className="links">
-                        <li onClick={() => navigate("/HomePage")}>Home</li>
-                        <li onClick={() => setViewLandingContact(true)} >Contact Us</li>
-                        <li  >Invest  </li>
-                        <li className='login-button'><StartLogin /></li>
+                        <li onClick={() => setViewLandingContact(true)}>Contact Us</li>
+                        <li >Invest  </li>
                     </ul>
 
                 </div>
-            </header>
+            </header> */}
+
+            {/* <li onClick={() => navigate("/HomePage")}>Home</li> */}
+            {/* <li className='login-button'><StartLogin /></li> */}
+
+            {/*
             <div className="content">
                 <div className="container">
                     <div className="info">
@@ -46,8 +85,10 @@ const LandingPage = (props) => {
                         <img src={apartImg} alt="Apartment Image" />
                     </div>
                 </div>
-            </div>
-            <div className='footer'>
+            </div> */}
+
+
+            {/* <div className='footer'>
                 <div className="footer-items">
                     <span className='titles'>About Us</span>
                     <span className='item'>
@@ -70,7 +111,7 @@ const LandingPage = (props) => {
                         <li>If registering as an Agent, click on Agents, register with the appropriate documents and start listing</li>
                     </span>
                 </div>
-            </div>
+            </div> */}
             {/* <CustomDialog
                 agentVisible={agentVisible}
                 setAgentVisible={setAgentVisible}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "primeflex/primeflex.css"
